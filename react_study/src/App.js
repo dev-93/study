7f1bf56d9bab9002208d8a546c4f8e81d207beda
dev-93 <cjb940103@gmail.com>
2019-11-30 23:12:56 +0900

    set App component's MyName props

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
 import MyName from './MyName';
-import './App.css';
 
 class App extends Component {
   render(){
-    return (
-      <div>
-        {/* 리액트 
-        안에서 주석처리 하는 법 */}
-        {/*  리액트 안에서 주석처리 하는 법  */}
-        <h1>리액트</h1>
-      </div>
-    )
+     return <MyName name="리액트" />;
   }
 }
 
