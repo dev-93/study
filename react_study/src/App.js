17aa417190ddc78b913b485053f698d26f55b34d
dev-93 <cjb940103@gmail.com>
2019-11-30 22:56:07 +0900

    how to use comments in React

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -5,8 +5,11 @@ import './App.css';
 class App extends Component {
   render(){
     return (
-      <div className="App">
-        안녕하세요!
+      <div>
+        {/* 리액트 
+        안에서 주석처리 하는 법 */}
+        {/*  리액트 안에서 주석처리 하는 법  */}
+        <h1>리액트</h1>
       </div>
     )
   }
