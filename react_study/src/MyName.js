d32789a9d5d5fcd7f777b1e951fbc8889e58330a
dev-93 <cjb940103@gmail.com>
2019-11-30 23:21:10 +0900

    when create a functional component, you don't have to load it at the top of your code

--- react_study/src/MyName.js
+++ react_study/src/MyName.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class MyName extends Component{
+const MyName = ({ name }) => {
+	return (
+		<div>
+			안녕하세요 ! 제 이름은 { name } 입니다.
+		</div>
+	)
+}
 
-	static defaultProps = {
-		name: '기본이름'
-	}
-	render() {
-		return(
-			<div>
-				안녕하세요! 제 이름은 <b>{this.props.name}</b> 입니다.
-			</div>
-		)
-	}
+MyName.defaultProps = {
+	name: 'taenam'
 }
 
 export default MyName;
