0800f25ba507ec3a0657009814ce2983223fb361
dev-93 <cjb940103@gmail.com>
2019-12-01 14:23:48 +0900

    added close and open function

--- publishing/Accordian/js/creactive_accordian.js
+++ publishing/Accordian/js/creactive_accordian.js
@@ -9,3 +9,13 @@ card.forEach(function (el) {
   el.addEventListener("click", openCard);
 });
 
+function closeCard(event) {
+  event.stopPropagation();
+  event.target.closest(".card").classList.add("closed");
+}
+
+function openCard(event) {
+  if (event.currentTarget.classList.contains("closed")) {
+    event.currentTarget.classList.remove("closed");
+  }
+}
