c8b04362c02e7f6a57ca05560e09ffff36b1391e
dev-93 <cjb940103@gmail.com>
2019-12-01 14:23:07 +0900

    added click event

--- publishing/Accordian/js/creactive_accordian.js
+++ publishing/Accordian/js/creactive_accordian.js
@@ -1,3 +1,11 @@
-const closeBtn = document.querySelectorAll(".close"), 
-card = document.querySelectorAll(".card");
+const closeBtn = document.querySelectorAll(".close"),
+  card = document.querySelectorAll(".card");
+
+closeBtn.forEach(function (el) {
+  el.addEventListener("click", closeCard);
+});
+
+card.forEach(function (el) {
+  el.addEventListener("click", openCard);
+});
 
