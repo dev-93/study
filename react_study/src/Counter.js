4dc9d56606545881e6c3349fa901dd45a0102bf9
dev-93 <cjb940103@gmail.com>
2020-02-25 23:28:20 +0900

    함수형 업데이트, 값을 업데이트하는 함수를 파라미터로 넣어줌

--- react_study/src/Counter.js
+++ react_study/src/Counter.js
@@ -1,31 +1,24 @@
 import React, {useState} from 'react';
-// 리액트 패키지에서 useState 라는 함수를 불러옴
+
 const Counter = () => {
 
     const [number, setNumber] = useState(0);
-    // useState를 사용할 때에는 상태의 기본값을 파라미터로 넣어서 호출
-    // 이 함수를 호출해주면 배열이 반한됨
-    // 첫번째 원소(number)는 현재 상태, 두번째 원소(setNumber)는 Setter 함수
-    // Setter 함수는 파라미터로 전달 받은 값을 최신 상태로 설정
-
-    // const numberState = useState(0);
-    // const number = numberState[0];
-    // const setNumber = numberState[1];
-    // 원래는 이렇게 해야하지만 배열 비구조화 할당을 통해 각 언소를 추출한 것이 다음과 같음  
     
     const onIncrease  = () => {
-        setNumber(number + 1);
+        setNumber(prevNumber => prevNumber + 1);
     }
 
     const onDecrease  = () => {
-        setNumber(number - 1);
+        setNumber(prevNumber => prevNumber - 1);
     }
 
+    // onIncrease 와 onDecrease 에서 setNumber 를 사용 할 때 그 다음 상태를 파라미터로 넣어준것이 아니라,
+    // 값을 업데이트 하는 함수를 파라미터로 넣어주었다.
     return(
         <>
             <h1>{number}</h1>
-            <button onClick={onIncrease }>+</button>
-            <button onClick={onDecrease }>-</button>
+            <button onClick={ onIncrease }>+</button>
+            <button onClick={ onDecrease }>-</button>
         </>
     )
 }
