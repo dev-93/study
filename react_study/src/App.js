45e2f3dc2db2ac95eb4634146682bd1edb6fa74a
dev-93 <cjb940103@gmail.com>
2020-02-25 22:36:18 +0900

    props.children = 컴포넌트 태그 사이에 넣은 값을 조회

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hello from "./Hello";
+import Wrapper from "./Wrapper"
 
 const App = () => {
   return (
     <>
-      <Hello color="red"/>    
+      <Wrapper>
+        <Hello name="react" color="blue"/>
+        <Hello color="red"/>
+      </Wrapper>
     </>
   );
 }
