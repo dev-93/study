0ed8869d941031c6f0b9c72a69c90c92a374461a
dev-93 <cjb940103@gmail.com>
2020-03-01 19:33:29 +0900

    기존의 연습 내용 지우고, TodoList 만들기

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -1,88 +1,17 @@
-import React, { useRef, useState, useMemo } from 'react';
-import UserList from './UserList';
-import CreateUser from './CreateUser';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 
-function countActiveUsers(users) {
-  console.log('활성 사용자 수를 세는중...');
-  return users.filter(user => user.active).length;
-}
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
 
 function App() {
-  const [inputs, setInputs] = useState({
-    username: '',
-    email: ''
-  });
-  const { username, email } = inputs;
-  const onChange = e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  };
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com',
-      active: true
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com',
-      active: false
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com',
-      active: false
-    }
-  ]);
-
-  const nextId = useRef(4);
-  const onCreate = () => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    setUsers(users.concat(user));
-
-    setInputs({
-      username: '',
-      email: ''
-    });
-    nextId.current += 1;
-  };
-
-  const onRemove = id => {
-    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-    // = user.id 가 id 인 것을 제거함
-    setUsers(users.filter(user => user.id !== id));
-  };
-  const onToggle = id => {
-    setUsers(
-      users.map(user =>
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
-  };
-  const count = useMemo(() => countActiveUsers(users), [users]);
-  // useMemo 의 첫번째 파라미터에는 어떻게 연산할지 정의하는 함수를 넣어주면 되고 
-  // 두번째 파라미터에는 deps 배열을 넣어주면 되는데, 이 배열 안에 넣은 내용이 바뀌면, 
-  // 우리가 등록한 함수를 호출해서 값을 연산해주고, 만약에 내용이 바뀌지 않았다면 이전에 연산한 값을 재사용하게 됩니다.
   return (
     <>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
-      <div>활성사용자 수 : {count}</div>
+      <GlobalStyle />
+      <div>뭐야아아아</div>
     </>
   );
 }
---
+++ react_study/src/components/TodoTemplate.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from "styled-components";
+
+
+function TodoTemplate({ children }) {
+    return <TodoTemplateBlock>{children}</TodoTemplateBlock>;
+  }
+  
+  export default TodoTemplate;
